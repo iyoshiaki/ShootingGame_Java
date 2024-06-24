99be11ae00b77db27a32f8e42294924551c15b88
yoshiaki <zeal23310053@fsgcl.jp>
2024-06-24 10:03:24 +0900

    12.1問題を追加

--- Shooting2/src/GameFrame.java
+++ Shooting2/src/GameFrame.java
@@ -14,6 +14,7 @@ public class GameFrame extends MyFrame {
 			GameWorld.player.move();
 			movePlayerBullets();
 			moveEnemies();
+			checkPlayerAndEnemies();
 			sleep(0.03);
 		}
 	}
@@ -32,13 +33,21 @@ public class GameFrame extends MyFrame {
 		}
 	}
 
-
-
 	public void moveEnemies() {
 		for (int i = 0; i < GameWorld.enemies.size(); i++) {
 			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+	}
+	public void checkPlayerAndEnemies() {
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
+			if (Math.abs(e.x - GameWorld.player.x)<=30 && 
+				Math.abs(e.y - GameWorld.player.y)<=30) {
+				System.out.println("やられた！");
+				GameWorld.player.y=-1000;
+			}
 		}
 	}
 }
